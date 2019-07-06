3fc022b974e5066d07e19821b13fe4ece294f693
cf2141 <cf2141@tgndomains.com>
2019-07-06 08:13:57 -0400

    added four echo files

---
+++ de.jwic.samples/src/de/jwic/samples/mechos01/echos01/Echo01.java
@@ -0,0 +1,40 @@
+package de.jwic.samples.mechos01.echo01;
+
+import de.jwic.base.Control;
+import de.jwic.base.ControlContainer;
+import de.jwic.base.IControlContainer;
+import de.jwic.base.ImageRef;
+import de.jwic.base.JavaScriptSupport;
+import de.jwic.controls.LabelControl;
+import de.jwic.controls.actions.IAction;
+import de.jwic.controls.HTMLElement;
+import de.jwic.controls.SelectableControl;
+
+//public class Echo01 extends HTMLElement {
+//public class Echo01 extends SelectableControl {
+public class Echo01 extends ControlContainer {
+	
+	private String echo = "not set";
+	private LabelControl echoName = null;
+	
+	public Echo01(IControlContainer container) {
+		super (container, null);
+	}
+	
+	public Echo01(IControlContainer container, String name) {
+		super (container, name);
+		this.setTemplateName("de.jwic.samples.mechos01.echo01.Echo01");
+		//echoName = new LabelControl(container, "echoName");
+		echoName = new LabelControl(this, "echoName");
+	}
+	
+	public void setEcho(String e) {
+		//echo = e;
+		System.out.println("Echo01.setEcho "+e);
+		echoName.setText( e );
+	}
+	
+	public String getEcho() {
+		return echoName.getText();
+	}
+}
