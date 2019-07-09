2accbba83e7cf89df44e906e88b82d3b8552c445
cf2141 <cf2141@tgndomains.com>
2019-07-08 21:36:52 -0400

    pruning, cleanup

--- de.jwic.samples/src/de/jwic/samples/magnechos/IEchoController.java
+++ de.jwic.samples/src/de/jwic/samples/magnechos/IEchoController.java
@@ -3,7 +3,6 @@ package de.jwic.samples.magnechos;
 import de.jwic.base.ControlContainer;
 import de.jwic.base.IControlContainer;
 
-
 public interface IEchoController {
 	public void init( IControlContainer container, String Name );
 	public void reset();
