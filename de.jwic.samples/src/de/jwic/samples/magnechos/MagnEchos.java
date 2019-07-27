deb520ac8c33a57eedd3ce11e4cb22e4a8ce00d6
cf2141 <cf2141@tgndomains.com>
2019-07-27 16:17:49 -0400

    removed printlns form MagnEchos

--- de.jwic.samples/src/de/jwic/samples/magnechos/MagnEchos.java
+++ de.jwic.samples/src/de/jwic/samples/magnechos/MagnEchos.java
@@ -22,35 +22,45 @@ public class MagnEchos extends Application {
 	public Control createRootControl(IControlContainer container) {
 		Page page = new Page(container);
 		
-		try {
-			JWicRuntime jWic = JWicRuntime.getJWicRuntime();
-			System.out.println("jWicRuntime context path = "+jWic.getContextPath());
-		} catch( Exception ex) {
-			ex.toString();
-		}
+		//try {
+		//	JWicRuntime jWic = JWicRuntime.getJWicRuntime();
+		//} catch( Exception ex) {
+		//	ex.toString();
+		//}
 		
 		SessionContext sc = this.getSessionContext();
-		/******
-		HashMap initParams = (HashMap)sc.getInitParameters();
-		Set keys = initParams.keySet();
-		System.out.println("-----.  session context key count "+keys.size());
-		for ( Iterator i = keys.iterator(); i.hasNext(); ) {
-			String key = (String)i.next();
-			String[] kValue = (String[])initParams.get(key);
-			System.out.println(" key("+key+") name[0]"+kValue[0]);
-		}
-		System.out.println("sc.getCallBackUR() "+sc.getCallBackURL());
-		*******/
-		System.out.println("sc.getURI "+sc.getURI());
 		
 		Echo_Control_Factory echoFactory = new Echo_Control_Factory();
 		
 		IEchoController echoWrapper = 
 		                  echoFactory.getEchoController( echoWrapperClassName );
 		
-		//echoWrapper.init( page, "echoController");
 		echoWrapper.init( page, sc.getURI() );
 		
 		return page;
 	}
+	
+	// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	//
+	//  saving this work of printing the session context 
+	//
+	public void printSessionContext() {
+		try {
+			SessionContext sc = this.getSessionContext();
+			HashMap initParams = (HashMap)sc.getInitParameters();
+			Set keys = initParams.keySet();
+			System.out.println("-----.  session context key count "+keys.size());
+			for ( Iterator i = keys.iterator(); i.hasNext(); ) {
+				String key = (String)i.next();
+				String[] kValue = (String[])initParams.get(key);
+				System.out.println(" key("+key+") name[0]"+kValue[0]);
+			}
+			System.out.println("sc.getCallBackUR() "+sc.getCallBackURL());
+			System.out.println("sc.getURI "+sc.getURI());
+		} catch (Exception ex) {
+			System.out.println("printSessionContext "+ex.toString());
+		}
+	}
+	
+	
 }
