06f5ee12bc975aeb6285a04667a8d251ec7a09b3
cf2141 <cf2141@tgndomains.com>
2019-08-21 21:50:34 -0400

    put back changes, need to start again

--- de.jwic.samples/src/de/jwic/samples/magnechos/AudioButton.js
+++ de.jwic.samples/src/de/jwic/samples/magnechos/AudioButton.js
@@ -3,9 +3,9 @@
 	afterUpdate: function(element) {
 	#if($control.visible)
 		var btn = JWic.$('btn_${control.controlID}');
-		//if (btn) {
-		//	JWic.controls.Button.initialize(btn, "${control.controlID}",$control.buildJsonOptions());
-		//}
+		if (btn) {
+			JWic.controls.Button.initialize(btn, "${control.controlID}",$control.buildJsonOptions());
+		}
 	#end
 	}
 }
