d7e8ee351e719d9b8b3c955fb2e9c9ee06824688
cf2141 <cf2141@tgndomains.com>
2019-08-27 07:19:56 -0400

    refining the mobile list presentation

--- de.jwic.samples/src/de/jwic/mobile02/demos/SelectMenuDemo02.java
+++ de.jwic.samples/src/de/jwic/mobile02/demos/SelectMenuDemo02.java
@@ -41,20 +41,94 @@ public final class SelectMenuDemo02 extends MobileDemoModule {
 		
 		MSelectmenu selectmenu = new MSelectmenu(container, "selectmenu");
 		selectmenu.setCorners(false);
-		ISelectElement firstone = selectmenu.addElement("firstitem", "First");
-		selectmenu.addElement("seconditem", "Second");
-		selectmenu.addElement("thirditem", "Third");
+		ISelectElement firstone = selectmenu.addElement("init-item", "select an echo");
+		//selectmenu.addElement("seconditem", "Second");
+		//selectmenu.addElement("thirditem", "Third");
 		selectmenu.setSelectedElement(firstone);
+		try {
+		  String url = "http://localhost:8080/01-amp3s/01-amp3s.json";
+		  URL obj = new URL(url);
+		  HttpURLConnection con = (HttpURLConnection)obj.openConnection();
+		  int responseCode = con.getResponseCode();
+		  System.out.println("\nSending 'GET' request to URL : " + url);
+		  System.out.println("Response Code : " + responseCode);
+		  BufferedReader in =new BufferedReader(
+		  new InputStreamReader(con.getInputStream()));
+		  String inputLine;
+		  StringBuffer response = new StringBuffer();
+		    while ((inputLine = in.readLine()) != null) {
+			  response.append(inputLine);
+		    } in .close();
+		    //print in String
+		    System.out.println(response.toString());
+		    JSONObject myresponse = new JSONObject(response.toString());
+		   
+			JSONArray arr = myresponse.getJSONArray("echo-list");
+			
+			//AudioButton[] arrayOButtons = new AudioButton[arr.length()];
+			
+			for (int i = 0; i < arr.length(); i++) {
+				System.out.println(" " + arr.get(i));
+				selectmenu.addElement(""+arr.get(i), ""+i+"-"+arr.get(i));
+		    /**
+				arrayOButtons[i] = new AudioButton(tlc);
+				arrayOButtons[i].addSelectionListener(
+					                       (new AudioSelection(""+arr.get(i))));
+				arrayOButtons[i].setTitle(""+arr.get(i));
+				arrayOButtons[i].setAudioLink("http://localhost:8080/01-amp3s/"+arr.get(i));
+			**/
+			}
+		   
+		} catch(Exception e) {
+			System.out.println(e);
+		}
 		
-		/****
 		MSelectmenu selectmenuinline = new MSelectmenu(container, "selectmenuinline");
 		selectmenuinline.setInline(true);
 		selectmenuinline.addElement("firstiteminline", "First Inline");
 		selectmenuinline.addElement("seconditeminline", "Second Inline");
 		selectmenuinline.addElement("thirditeminline", "Third Inline");
 		selectmenuinline.addElement("fourthiteminline", "Fourth Inline");
-		****/
+		try {
+		  String url = "http://localhost:8080/01-amp3s/01-amp3s.json";
+		  URL obj = new URL(url);
+		  HttpURLConnection con = (HttpURLConnection)obj.openConnection();
+		  int responseCode = con.getResponseCode();
+		  System.out.println("\nSending 'GET' request to URL : " + url);
+		  System.out.println("Response Code : " + responseCode);
+		  BufferedReader in =new BufferedReader(
+		  new InputStreamReader(con.getInputStream()));
+		  String inputLine;
+		  StringBuffer response = new StringBuffer();
+		    while ((inputLine = in.readLine()) != null) {
+			  response.append(inputLine);
+		    } in .close();
+		    //print in String
+		    System.out.println(response.toString());
+		    JSONObject myresponse = new JSONObject(response.toString());
+		   
+			JSONArray arr = myresponse.getJSONArray("echo-list");
+			
+			//AudioButton[] arrayOButtons = new AudioButton[arr.length()];
+			
+			for (int i = 0; i < arr.length(); i++) {
+				System.out.println(" " + arr.get(i));
+				selectmenuinline.addElement(""+arr.get(i), ""+i+"-"+arr.get(i));
+		    /**
+				arrayOButtons[i] = new AudioButton(tlc);
+				arrayOButtons[i].addSelectionListener(
+					                       (new AudioSelection(""+arr.get(i))));
+				arrayOButtons[i].setTitle(""+arr.get(i));
+				arrayOButtons[i].setAudioLink("http://localhost:8080/01-amp3s/"+arr.get(i));
+			**/
+			}
+		   
+		} catch(Exception e) {
+			System.out.println(e);
+		}
 		
+		
+		/*****
 		MSelectmenu selectmenumini = new MSelectmenu(container, "selectmenumini");
 		selectmenumini.setMini(true);
 		//selectmenumini.addElement("firstitemmini", "First Mini");
@@ -91,12 +165,13 @@ public final class SelectMenuDemo02 extends MobileDemoModule {
 					                       (new AudioSelection(""+arr.get(i))));
 				arrayOButtons[i].setTitle(""+arr.get(i));
 				arrayOButtons[i].setAudioLink("http://localhost:8080/01-amp3s/"+arr.get(i));
-			**/
+			**--------------------------------/
 			}
 		   
 		} catch(Exception e) {
 			System.out.println(e);
 		}
+		****/
 		
 		return container;
 	}
