fe105b856e21e73145bd71c7a25fd743576f6c5b
cf2141 <cf2141@tgndomains.com>
2019-09-07 17:32:17 -0400

    IndexAction ...

--- de.jwic.samples/src/de/jwic/mobile05/demos/EchoWindow.java
+++ de.jwic.samples/src/de/jwic/mobile05/demos/EchoWindow.java
@@ -7,8 +7,23 @@ import de.jwic.base.IControlContainer;
 
 import de.jwic.controls.ScrollableContainer;
 
-public class EchoWindow extends MobileDemoModule {
+import de.jwic.controls.AnchorLinkControl;
+import de.jwic.controls.actions.IAction;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class EchoWindow extends MobileDemoModule implements PropertyChangeListener {
 	
+    protected transient Log log = LogFactory.getLog(getClass());
+    
 	public EchoWindow() {
 		super("Echo Window");
 	}
@@ -26,5 +41,62 @@ public class EchoWindow extends MobileDemoModule {
 		return container;
 	}
 
+	
+    
+    public void actionPerformed(String actionId, String parameter) {
+        //System.out.println("pinnacle Index.actionPerformed actionId("+
+        //                             actionId+") parameter("+parameter+")");
+        int x = Integer.parseInt(parameter);
+        //nu.xom.Element guestData = (nu.xom.Element)guestList.getChild(x);
+        //System.out.println(guest.getFirstChildElement("DataCenter").getValue());
+        
+        if (actionId.equals("click")) {
+            /*****
+            IndexAction action = 
+                             (IndexAction)actionMap.get(new Integer(parameter));
+            if (action != null) {
+                if (action.isEnabled()) {
+                    setCorrectIndexActive(action);
+                    action.setActiveTrue();
+                    PropertyChangeEvent pce = new PropertyChangeEvent(
+                                        action,action.getTitle(),action,action);
+                    // This should be moved below so guestInstallColumn 
+                    // sits with guestInstallColumn
+                    actionListener.propertyChange(pce);
+                    ((PropertyChangeListener)
+                                        guestInstallColumn).propertyChange(pce);
+                    int guest_id = Integer.parseInt(action.getTitle());
+                    
+                    guestDetails.setGuestValues(guestData,guest_id);
+                    
+                    pce = new PropertyChangeEvent(action,"guest_index",action,action);
+                    ((PropertyChangeListener)guestInstallColumn).propertyChange(pce);
+                    
+                } else {
+                    log.warn("Action with the ID '" + parameter +
+                            "' was clicked but is disabled");
+                }
+            } else {
+                log.warn("Action with ID '" + parameter +
+                        "' does not exist");
+            }
+            *****/
+        }
+    }
+    
+    
+    //--------------------------------------------------------------------------
+    public void propertyChange(PropertyChangeEvent event) {
+         System.out.println("pinnacle.Index.propertyChange "+event.getPropertyName());
+        if(event.getPropertyName().equals("sow_data_change")) {
+            //System.out.println("pinnacle.Index.propertyChange .. sow_data_change .. event");
+            //reset();
+            //requireRedraw();
+        } else {
+            log.debug(" something not expected happned ..........");
+        }
+    }
+	
+	
 	
 }
---
+++ de.jwic.samples/src/de/jwic/mobile05/demos/IndexAction.java
@@ -0,0 +1,85 @@
+package de.jwic.mobile05.demos;
+
+import de.jwic.controls.actions.IAction;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import de.jwic.base.ImageRef;
+	
+public class IndexAction implements IAction {
+    
+    protected transient Log log = LogFactory.getLog(getClass());
+    
+    private String name;
+    private boolean active = false;
+    
+    public IndexAction(int x) {
+        name = Integer.toString(x);
+    }
+    
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+    }
+    
+    public void removePropertyChangeListener(PropertyChangeListener lisetener) {
+    }
+    
+    public String getTitle() {
+        return name;
+    }
+    
+    public String getImagePath() {
+        return "";
+    }
+    
+    public boolean isEnabled() {
+        return true;
+    }
+    
+    public boolean isVisible() {
+        return true;
+    }
+    
+    public void setActiveFalse(){
+        log.debug("setActiveFalse");
+        active = false;
+    }
+    
+    public void setActiveTrue(){
+        log.debug("setActiveTrue");
+        active = true;
+    }
+    
+    public boolean isActive() {
+        log.debug("isActive "+active);
+        return active;
+    }
+    
+    public void run() {
+        log.debug("  present guest " + name);
+    }
+   	public String getTooltip(){
+   		return "";
+   	}
+	public void setTooltip(String tooltip){
+	}
+	public void setVisible(boolean visible){
+	}
+	public void setEnabled(boolean enabled){
+	}
+	public void setIconDisabled(ImageRef iconDisabled){
+	}
+	public ImageRef getIconDisabled(){
+		return null;
+	}
+	public void setIconEnabled(ImageRef iconEnabled){
+	}
+	public ImageRef getIconEnabled(){
+		return null;
+	}
+	public void setTitle(String title){
+	}
+}
+
