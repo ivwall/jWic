55370d7f4577e0df4d4632eb8db58d3cdfa9d67b
cf2141 <cf2141@tgndomains.com>
2019-10-06 21:05:50 -0400

    improvements to be sure

--- de.jwic.samples/src/de/jwic/mobile09/demos/AudioSelection.java
+++ de.jwic.samples/src/de/jwic/mobile09/demos/AudioSelection.java
@@ -5,10 +5,13 @@ import de.jwic.events.SelectionEvent;
 import de.jwic.controls.mobile04.MButton;
 import de.jwic.controls.mobile08.MCheckBox;
 
+import de.jwic.mobile09.demos.audio.Player;
+
 public class AudioSelection implements SelectionListener {
 	private String    audioTitle = "not set";
 	private EchoLabel echoLabel = null;
 	private MCheckBox ckBoxAsLbl = null;
+	private Player    player = null;
 	public AudioSelection(){
 	}
 	public AudioSelection(String t){
@@ -23,12 +26,24 @@ public class AudioSelection implements SelectionListener {
 		echoLabel = lbl;
 		ckBoxAsLbl = ckbx;
 	}
+	public AudioSelection(String t, EchoLabel lbl, MCheckBox ckbx, Player p){
+		audioTitle = t;
+		echoLabel = lbl;
+		ckBoxAsLbl = ckbx;
+		player = p;
+	}
 	public void objectSelected(SelectionEvent event){
 		System.out.println("objectSelected = "+audioTitle);
 		try {
 			ckBoxAsLbl.setLabel(audioTitle);
 		} catch( Exception ex) {
 			System.out.println("AudioSelection.objectSelected "+ex.toString());
+		}
+		try {
+			player.setAudio(audioTitle);
+			player.requireRedraw();
+		} catch( Exception x2 ) {
+			System.out.println("AudioSelection.objectSelected "+x2.toString());
 		}
 	}
 	public void setTitle(String t){
--- de.jwic.samples/src/de/jwic/mobile09/demos/audio/Player.java
+++ de.jwic.samples/src/de/jwic/mobile09/demos/audio/Player.java
@@ -7,7 +7,17 @@ import de.jwic.base.Control;
 
 
 public class Player extends Control {
+	
+	private String strAudio = "";
 	public Player(IControlContainer parent, String a) {
 		super(parent,a);
+	}
+	public void setAudio(String a) {
+		System.out.println("Player.setAudio "+a);
+		strAudio = a;
+	}
+	public String getAudio() {
+		System.out.println("Player.setAudio "+"http://localhost:8080/01-amp3s/"+strAudio);
+		return "http://localhost:8080/01-amp3s/"+strAudio;
 	}
 }
