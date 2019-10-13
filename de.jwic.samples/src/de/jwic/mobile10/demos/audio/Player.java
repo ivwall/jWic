7c96e9e47a724e5681a3382533997d8ed2e2246f
cf2141 <cf2141@tgndomains.com>
2019-10-13 09:48:29 -0400

    mediaelement-and-player.js and demo.js, tricky scope and visiblity

--- de.jwic.samples/src/de/jwic/mobile10/demos/audio/Player.java
+++ de.jwic.samples/src/de/jwic/mobile10/demos/audio/Player.java
@@ -14,9 +14,18 @@ public class Player extends Control {
 	public void setAudio(String a) {
 		System.out.println("Player.setAudio "+a);
 		strAudio = a;
+		try {
+			//System.out.println(");
+			this.requireRedraw();
+		} catch (Exception ex) {
+		}
 	}
 	public String getAudio() {
 		System.out.println("Player.setAudio "+"http://localhost:8080/01-amp3s/"+strAudio);
+		try {
+			this.requireRedraw();
+		} catch (Exception ex) {
+		}
 		return "http://localhost:8080/01-amp3s/"+strAudio;
 	}
 	public String getTitle() {
