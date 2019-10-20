c0c148da70a84291382a5f049fce963f30cdb527
cf2141 <cf2141@tgndomains.com>
2019-10-20 09:44:42 -0400

    developing an AudioController based on rnd AudioPlayer

---
+++ de.jwic.samples/src/de/jwic/mobile10/demos/audio/AudioController.java
@@ -0,0 +1,22 @@
+package de.jwic.mobile10.demos.audio;
+
+
+import de.jwic.base.Control;
+import de.jwic.base.ControlContainer;
+import de.jwic.base.IControlContainer;
+//import de.jwic.controls.mobile04.MSelectmenu;
+import de.jwic.data.ISelectElement;
+//import de.jwic.mobile10.MobileDemoModule;
+
+public class AudioController extends ControlContainer {
+	
+	private ControlContainer container;
+	
+	/**
+	 * Constructor.
+	 * @param parent
+	 */
+	public AudioController (IControlContainer parent) {
+		super(parent);
+	}
+}
---
+++ de.jwic.samples/src/de/jwic/mobile10/demos/audio/PlayPause.java
@@ -0,0 +1,7 @@
+package de.jwic.mobile10.demos.audio;
+
+
+public class PlayPause {
+	public PlayPause() {
+	}
+}
