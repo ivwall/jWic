6311a54aabbf34786c5b806f72627c3446e497cb
cf2141 <cf2141@tgndomains.com>
2019-10-25 07:49:52 -0400

    javascript, java anotations

--- de.jwic.samples/src/de/jwic/mobile10/demos/audio/PlayPause.java
+++ de.jwic.samples/src/de/jwic/mobile10/demos/audio/PlayPause.java
@@ -4,7 +4,9 @@ import de.jwic.controls.SelectableControl;
 import de.jwic.base.Control;
 import de.jwic.base.ControlContainer;
 import de.jwic.base.IControlContainer;
+import de.jwic.base.JavaScriptSupport;
 
+@JavaScriptSupport
 public class PlayPause extends SelectableControl {
 	
 	public PlayPause(IControlContainer container) {
